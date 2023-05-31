8c6ec9eb7b2aabf3ce14076e8b8040ae714a2174
niiko <nicolas.rocagel@epitech.eu>
2023-05-31 17:57:45 +0200

    update

--- main-lovers/src/app/page.tsx
+++ main-lovers/src/app/page.tsx
@@ -3,7 +3,7 @@ import NavBar from "./components/NavBar";
 import HomeScreen from "./components/HomeScreen";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
-import Link from "next/link";
+// import Link from "next/link";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
